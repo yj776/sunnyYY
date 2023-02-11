00b55c5a4a073364c7964cf4d25fa17fab3412ae
Amii Jiang <amii@MacBook-Pro.fios-router.home>
2023-02-11 15:03:21 -0500

    added call us button in contact us

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -11,9 +11,13 @@ export default function ContactUs(){
             <br></br>
                 <p>Tel: 718-666-7040</p>
                 <p>Tel2: 917-498-8216</p>
+                <a href="tel:7186667040"><Button variant="primary">Call Us</Button></a>
+                <br></br>
+                <br></br>
                 <p>Email: sunnymoving@hotmail.com</p>
                 <p><a href= "www.sunnymovingny.com">www.sunnymoving.com</a></p>
-            <a href="mailto:sunnymoving@hotmail.com"><Button variant="primary">Contact Us</Button></a>
+            <a href="mailto:sunnymoving@hotmail.com"><Button variant="primary">Email Us</Button></a>
+            <br></br>
             <br></br>
                 <img src={move2} alt = "Sigh" />
             
